5f6a7b3edaa4c922160d2a5498be3fdddb90b282
Alexander Peters <abp@omega.local>
2019-10-16 15:49:30 -0400

    Label format finalized

--- src/LabelWriter.java
+++ src/LabelWriter.java
@@ -10,45 +10,63 @@ import javax.print.PrintService;
 
 public class LabelWriter {
 
-    public static final String PRINTERNAME = "DYMO LabelWriter";
-    PrinterJob printerJob = PrinterJob.getPrinterJob();
-    PageFormat pageFormat = printerJob.defaultPage();
-    Paper paper = new Paper();
+	public static final String PRINTERNAME = "DYMO LabelWriter 450 Turbo";
+	PrinterJob printerJob = PrinterJob.getPrinterJob();
+	PageFormat pageFormat = printerJob.defaultPage();
+	Paper paper = new Paper();
 
-    public void printLabel() {
-        final double widthPaper = (1.125 * 72);
-        final double heightPaper = (3.5 * 72);
-        
-        paper.setSize(widthPaper, heightPaper);
-        paper.setImageableArea(0, 0, widthPaper, heightPaper);
-        pageFormat.setPaper(paper);
-        pageFormat.setOrientation(PageFormat.LANDSCAPE);
-        PrintService[] printService = PrinterJob.lookupPrintServices();
+	public void printLabel() {
+		final double widthPaper = (1.125 * 72);
+		final double heightPaper = (3.5 * 72);
 
-        for (int i = 0; i < printService.length; i++) {
-            if (printService[i].getName().contains(PRINTERNAME)) {
-                try {
-                    printerJob.setPrintService(printService[i]);
-                    printerJob.setPrintable(new Printable() {
-                        @Override
-                        public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
-                                throws PrinterException {
-                            if (pageIndex < 1) {
-                                Graphics2D g = (Graphics2D) graphics;
-                                g.translate(20, 10);                          
-                                g.setFont(new Font(g.getFont().getFontName(), g.getFont().getStyle(), 60));
-                                g.drawString("Ω✓", -5, 45);
-                                return PAGE_EXISTS;
-                            } else {
-                                return NO_SUCH_PAGE;
-                            }
-                        }
-                    }, pageFormat); // The 2nd param is necessary for printing into a label width a right landscape format.
-                    printerJob.print();
-                } catch (PrinterException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		paper.setSize(widthPaper, heightPaper);
+		paper.setImageableArea(0, 0, widthPaper, heightPaper);
+		pageFormat.setPaper(paper);
+		pageFormat.setOrientation(PageFormat.LANDSCAPE);
+		PrintService[] printService = PrinterJob.lookupPrintServices();
+
+		for (int i = 0; i < printService.length; i++) {
+			if (printService[i].getName().equals(PRINTERNAME)) {
+				try {
+					printerJob.setPrintService(printService[i]);
+					printerJob.setPrintable(new Printable() {
+						@Override
+						//TODO: Pass strings printed on the label as parameters
+						public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
+								throws PrinterException {
+							if (pageIndex < 1) {
+								Graphics2D g = (Graphics2D) graphics;
+								g.translate(20, 10);
+
+								// Add Omega Check to start of label
+								g.setFont(new Font(g.getFont().getFontName(), g.getFont().getStyle(), 60));
+								g.drawString("Ω✓", -5, 48);
+
+								// Add Label Data
+								g.setFont(new Font(g.getFont().getFontName(), g.getFont().getStyle(), 9));
+								// Names should be no longer that 11 charachters including the seperating space,
+								// Names are capitalized first and last
+								g.drawString("QC Inspector: Joe Schmoqw", 90, 10);
+								g.drawString("Operator: Jane Schmoqwert", 90, 24);
+								// Work orders should always be 6 digits long, and there will not be more than
+								// 999 boxes per WO
+								g.drawString("WO / BoxID: 123456 / 321", 90, 38);
+								// Date format "Date: dd/mm/yyyy"
+								g.drawString("Date: 10/10/2019", 90, 52);
+
+								return PAGE_EXISTS;
+							} else {
+								return NO_SUCH_PAGE;
+							}
+						}
+					}, pageFormat); // The 2nd param is necessary for printing into a label width a right landscape
+									// format.
+					printerJob.print();
+				} catch (PrinterException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+	}
 }
--- src/Webcam_Capture.java
+++ src/Webcam_Capture.java
@@ -20,75 +20,76 @@ import javax.swing.JTextField;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamResolution;
 
+//TODO: Author, citations, and documentation
 public class Webcam_Capture {
-	static boolean	wPressed = false;
-	static boolean	ePressed = false;
+	static boolean wPressed = false;
+	static boolean ePressed = false;
+	static LabelWriter labelwriter = new LabelWriter();
 
 	// Main method
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		// Declarations and Instantiations
 		Webcam webcam = Webcam.getDefault();
-		JFrame frame = new JFrame();	
+		JFrame frame = new JFrame();
 		JPanel contentContainerPanel = new JPanel();
 		ImagePanel imagePanel;
 		JPanel textPanel;
 		JTextField textField;
-        BufferedImage image = null;
-        boolean newImage = false;
-        
-        // Initialize camera
-        initWebCam(webcam);
-		
+		BufferedImage image = null;
+		boolean newImage = false;
+
+		// Initialize camera
+		initWebCam(webcam);
+
 		// Frame definition
 		frame.setTitle("Basic Quality Inspection Program");
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
 		// Image Panel
-		BufferedImage blankImage = new BufferedImage(WebcamResolution.HD.getSize().width, WebcamResolution.HD.getSize().height, BufferedImage.TRANSLUCENT);	
+		BufferedImage blankImage = new BufferedImage(WebcamResolution.HD.getSize().width,
+				WebcamResolution.HD.getSize().height, BufferedImage.TRANSLUCENT);
 		imagePanel = new ImagePanel(blankImage);
-		
+
 		// Text Panel
 		textPanel = new JPanel();
-        textField = new JTextField(35);
-        textField.setText("Press W to update image and E to approve image.");
-        textField.setFont(textField.getFont().deriveFont(50f));
-        textField.setHorizontalAlignment(JTextField.CENTER);
-        textField.setEditable(false);
-        textPanel.add(textField, BorderLayout.CENTER);
+		textField = new JTextField(35);
+		textField.setText("Press W to update image and E to approve image.");
+		textField.setFont(textField.getFont().deriveFont(50f));
+		textField.setHorizontalAlignment(JTextField.CENTER);
+		textField.setEditable(false);
+		textPanel.add(textField, BorderLayout.CENTER);
 
-        // Combine in Content Panel
-        contentContainerPanel.add(imagePanel, BorderLayout.NORTH);
-        contentContainerPanel.add(textPanel, BorderLayout.SOUTH);
-        
-        // Add to Frame and make visible
-        frame.add(contentContainerPanel, BorderLayout.CENTER);
-        frame.setVisible(true);
-        frame.addWindowListener(new WindowAdapter() {
-        	@Override
-        	public void windowClosing(WindowEvent e) {
-        		LabelWriter labelwriter = new LabelWriter();
-        		labelwriter.printLabel();//"Amico QC Checked", "White Punched Extrusion", "Line: ", "Die: ", "Production Run: ", "17", "256", "190", "Date: 10/9/19", "Time: 2:10PM", "QC Inspector: APeters", "Box 1: ", "32 Count");
-        		// Close WebCam at end of program
-        		closeWebCam(webcam);
-        		// Exit program
-        		System.exit(0);
-        	}
-        	 
-        });
-		
-        // Update on Key Press
-		while(true) {
-	        KeyListen();
-			if(wPressed) {
+		// Combine in Content Panel
+		contentContainerPanel.add(imagePanel, BorderLayout.NORTH);
+		contentContainerPanel.add(textPanel, BorderLayout.SOUTH);
+
+		// Add to Frame and make visible
+		frame.add(contentContainerPanel, BorderLayout.CENTER);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// Close WebCam at end of program
+				closeWebCam(webcam);
+				// Exit program
+				System.exit(0);
+			}
+
+		});
+
+		// Update on Key Press
+		while (true) {
+			KeyListen();
+			if (wPressed) {
 				image = captureImage(webcam);
 				contentContainerPanel.remove(imagePanel);
 				imagePanel = new ImagePanel(image);
-		        contentContainerPanel.add(imagePanel, BorderLayout.NORTH);
-		        contentContainerPanel.add(textPanel, BorderLayout.SOUTH);
-		        contentContainerPanel.validate();	
-		        newImage = true;
+				contentContainerPanel.add(imagePanel, BorderLayout.NORTH);
+				contentContainerPanel.add(textPanel, BorderLayout.SOUTH);
+				contentContainerPanel.validate();
+				newImage = true;
 			}
-			if(ePressed && image != null && newImage) {
+			if (ePressed && image != null && newImage) {
 				Approved(image);
 				newImage = false;
 			}
@@ -99,83 +100,84 @@ public class Webcam_Capture {
 			}
 		}
 	}
-	
+
 	// Init WebCam
 	private static void initWebCam(Webcam webcam) {
-		Dimension[] nonStandardResolutions = new Dimension[] {
-				WebcamResolution.PAL.getSize(),
-				WebcamResolution.HD.getSize(),
-				new Dimension(2000, 1000),
-				new Dimension(1000, 500)
-		};
-			
+		Dimension[] nonStandardResolutions = new Dimension[] { WebcamResolution.PAL.getSize(),
+				WebcamResolution.HD.getSize(), new Dimension(2000, 1000), new Dimension(1000, 500) };
+
 		webcam.setCustomViewSizes(nonStandardResolutions);
 		webcam.setViewSize(WebcamResolution.HD.getSize());
 		webcam.open();
 	}
-	
+
 	// Capture Images
-	private static BufferedImage captureImage(Webcam webcam) {	
+	private static BufferedImage captureImage(Webcam webcam) {
 		return webcam.getImage();
 	}
-	
+
 	// Close WebCam
 	private static void closeWebCam(Webcam webcam) {
-		webcam.close();	
+		webcam.close();
 	}
-	
+
 	// Key listener
 	private static void KeyListen() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
 			@Override
-		 	public boolean dispatchKeyEvent(KeyEvent event) {
+			public boolean dispatchKeyEvent(KeyEvent event) {
 				switch (event.getID()) {
-					case KeyEvent.KEY_PRESSED:
-						if (event.getKeyCode() == KeyEvent.VK_W)
-		     				Webcam_Capture.wPressed = true; 
-						if (event.getKeyCode() == KeyEvent.VK_E)
-		     				Webcam_Capture.ePressed = true;
-						break;
-					case KeyEvent.KEY_RELEASED:
-						if (event.getKeyCode() == KeyEvent.VK_W)
-		    				Webcam_Capture.wPressed = false;
-						if (event.getKeyCode() == KeyEvent.VK_E)
-		    				Webcam_Capture.ePressed = false;
+				case KeyEvent.KEY_PRESSED:
+					if (event.getKeyCode() == KeyEvent.VK_W)
+						Webcam_Capture.wPressed = true;
+					if (event.getKeyCode() == KeyEvent.VK_E)
+						Webcam_Capture.ePressed = true;
+					break;
+				case KeyEvent.KEY_RELEASED:
+					if (event.getKeyCode() == KeyEvent.VK_W)
+						Webcam_Capture.wPressed = false;
+					if (event.getKeyCode() == KeyEvent.VK_E)
+						Webcam_Capture.ePressed = false;
 					break;
 				}
 				return false;
 			}
-		});		
+		});
 	}
-	
+
 	// Image Approved
 	private static void Approved(BufferedImage image) {
 		String filePath = getPicturePath(null, null, null, null);
+		
 		// Save a local copy of the image
 		try {
 			ImageIO.write(image, "PNG", new File(filePath));
 		} catch (IOException e) {
 			e.printStackTrace();
-		}	
+		}
 		
-		PrintLabel();
+		// Print The Label
+		labelwriter.printLabel();
 	}
-	
-	// Print The Label
-	private static void PrintLabel() {
-		
-	}
-	
+
+	//TODO
 	// Get date as a string
 	private static String getDate() {
 		DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 		Date date = new Date();
 		return sdf.format(date);
 	}
-	
+
+	//TODO
 	// Get appropriate save address
-	private static String getPicturePath(String saveLocation, String lineNumber, String dieNumber, String productionRun) {
-		
+	private static String getPicturePath(String saveLocation, String lineNumber, String dieNumber,
+			String productionRun) {
 		return "C:\\Users\\abp\\Desktop\\test.PNG";
+	}
+	
+	//TODO
+	// Modify the message in the window
+	private static void modifyMessage(String newMessage) {
+		
 	}
 }
