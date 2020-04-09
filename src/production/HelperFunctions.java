3642dbdb6cf50533b947b0b6e0e5f3d96dabb426
Alexander Peters <abp@omega.local>
2020-04-09 11:37:55 -0400

    Cleaned: (This is the archived version on O:\)

--- src/production/HelperFunctions.java
+++ src/production/HelperFunctions.java
@@ -1,4 +1,4 @@
-package main;
+package production;
 
 public class HelperFunctions {
 	public static String fourDigitBoxIDConversion(String boxID) throws Exception {
--- src/production/LabelWriter.java
+++ src/production/LabelWriter.java
@@ -1,4 +1,4 @@
-package main;
+package production;
 
 import java.awt.Font;
 import java.awt.Graphics;
